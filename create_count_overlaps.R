a28c2bfc9d9dcc97f7eafbe0e1a0386074af65ab
Mohamad Bairakdar <mdanb@umich.edu>
2022-06-13 20:00:04 -0400

    when count overlaps was acting weird with saving

--- create_count_overlaps.R
+++ create_count_overlaps.R
@@ -5,33 +5,48 @@ library(data.table)
 library(dplyr)                                                                  
 library(tibble)                                                                 
 
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) != 1) {
+  stop("One argument must be supplied", call.=FALSE)
+} else {
+  CELL_NUMBER_FILTER = args[1]
+} 
+
 load('/ahg/regevdata/projects/ICA_Lung/Bruno/celloforigin_prj/hg19.1Mb.ranges.Polak.Nature2015.RData')
-interval.ranges                                                                 
+#load('hg19.1Mb.ranges.Polak.Nature2015.RData')
+
+interval.ranges                                 
 
 dir.create("count_overlap_data")                                                
 
 metadata = read.table("raw_dir/metadata/GSE184462_metadata.tsv", sep="\t", header=TRUE)  
 
 for (file in setdiff(list.files("raw_dir/bed_files/"), list.dirs("raw_dir/bed_files", 
-		     recursive = FALSE, full.names = FALSE))) {                                          
-        filename = paste("count_overlaps", paste(file_path_sans_ext(file, TRUE),
+		     recursive = FALSE, full.names = FALSE))) {                                    
+  filename = paste("count_filter", CELL_NUMBER_FILTER,  
+                   "count_overlaps", paste(file_path_sans_ext(file, TRUE),
 	                 "rds", sep="."), sep="_")
-	if (!file.exists(filename)) {                                           
-                sample = import(paste("raw_dir", "bed_files", file, sep="/"), format="bed")  
-                sample_name = str_extract(file, pattern="_.*_rep")              
-                sample_name = substr(sample_name, 2, nchar(sample_name) - 4)    
+	if (!file.exists(filename)) {
+		print(paste("Processing", file, sep= " "))
+		sample = import(paste("raw_dir", "bed_files", file, sep="/"), format="bed")
+		if (grepl("frontal_cortex", file)) {
+			sample_name = "frontal_cortex"
+		} else {
+			sample_name = str_extract(file, pattern="_.*_SM")              
+			sample_name = substr(sample_name, 2, nchar(sample_name) - 3)    
+                }
                 filtered_metadata = metadata[metadata$sample %like% sample_name, ]
                 sample_cellIDs = filtered_metadata[["cellID"]]                  
                 sample_barcodes_in_metadata = unlist(lapply(sample_cellIDs, function(x) unlist(strsplit(x, '\\+'))[2]))
                 sample = sample[sample$name %in% sample_barcodes_in_metadata]   
                 sample_idx_in_metadata = match(sample$name, sample_barcodes_in_metadata)
                 sample$cell_type = filtered_metadata[unlist(sample_idx_in_metadata), "cell.type"]
-                sample = as.tibble(sample)                                      
+                sample = as.tibble(sample)                                 
                 counts_per_cell_type = sample %>%                               
                                        group_by(cell_type) %>%                 
                                        summarize(n_cells = n_distinct(name))    
 
-                cell_type_keep = counts_per_cell_type[counts_per_cell_type$n_cells >= 10, ]$cell_type
+                cell_type_keep = counts_per_cell_type[counts_per_cell_type$n_cells >= CELL_NUMBER_FILTER, ]$cell_type
                 sample = sample %>%                                             
                          filter(cell_type %in% cell_type_keep)                  
 
