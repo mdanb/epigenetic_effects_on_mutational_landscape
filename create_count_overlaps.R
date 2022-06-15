13954c51b020459202714c92392515b0311cd992
Mohamad Bairakdar <mdanb@umich.edu>
2022-06-15 11:44:46 -0400

    change style

--- create_count_overlaps.R
+++ create_count_overlaps.R
@@ -9,11 +9,11 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args) != 1) {
   stop("One argument must be supplied", call.=FALSE)
 } else {
-  CELL_NUMBER_FILTER = args[1]
+  CELL_NUMBER_FILTER = as.integer(args[1])
 } 
 
-load('/ahg/regevdata/projects/ICA_Lung/Bruno/celloforigin_prj/hg19.1Mb.ranges.Polak.Nature2015.RData')
-#load('hg19.1Mb.ranges.Polak.Nature2015.RData')
+#load('/ahg/regevdata/projects/ICA_Lung/Bruno/celloforigin_prj/hg19.1Mb.ranges.Polak.Nature2015.RData')
+load('hg19.1Mb.ranges.Polak.Nature2015.RData')
 
 interval.ranges                                 
 
@@ -31,28 +31,29 @@ for (file in setdiff(list.files("raw_dir/bed_files/"), list.dirs("raw_dir/bed_fi
 		sample = import(paste("raw_dir", "bed_files", file, sep="/"), format="bed")
 		if (grepl("frontal_cortex", file)) {
 			sample_name = "frontal_cortex"
-		} else {
+		} 
+		else {
 			sample_name = str_extract(file, pattern="_.*_SM")              
 			sample_name = substr(sample_name, 2, nchar(sample_name) - 3)    
-                }
-                filtered_metadata = metadata[metadata$sample %like% sample_name, ]
-                sample_cellIDs = filtered_metadata[["cellID"]]                  
-                sample_barcodes_in_metadata = unlist(lapply(sample_cellIDs, function(x) unlist(strsplit(x, '\\+'))[2]))
-                sample = sample[sample$name %in% sample_barcodes_in_metadata]   
-                sample_idx_in_metadata = match(sample$name, sample_barcodes_in_metadata)
-                sample$cell_type = filtered_metadata[unlist(sample_idx_in_metadata), "cell.type"]
-                sample = as.tibble(sample)                                 
-                counts_per_cell_type = sample %>%                               
-                                       group_by(cell_type) %>%                 
-                                       summarize(n_cells = n_distinct(name))    
+    }
+    filtered_metadata = metadata[metadata$sample %like% sample_name, ]
+    sample_cellIDs = filtered_metadata[["cellID"]]                  
+    sample_barcodes_in_metadata = unlist(lapply(sample_cellIDs, function(x) unlist(strsplit(x, '\\+'))[2]))
+    sample = sample[sample$name %in% sample_barcodes_in_metadata]   
+    sample_idx_in_metadata = match(sample$name, sample_barcodes_in_metadata)
+    sample$cell_type = filtered_metadata[unlist(sample_idx_in_metadata), "cell.type"]
+    sample = as.tibble(sample)                                 
+    counts_per_cell_type = sample %>%                               
+                           group_by(cell_type) %>%                 
+                           summarize(n_cells = n_distinct(name))    
 
-                cell_type_keep = counts_per_cell_type[counts_per_cell_type$n_cells >= CELL_NUMBER_FILTER, ]$cell_type
-                sample = sample %>%                                             
-                         filter(cell_type %in% cell_type_keep)                  
+    cell_type_keep = counts_per_cell_type[counts_per_cell_type$n_cells >= CELL_NUMBER_FILTER, ]$cell_type
+    sample = sample %>%                                             
+             filter(cell_type %in% cell_type_keep)                  
 
-                grl_in = sample %>%                                             
-		         group_split(cell_type) %>%                             
-			 lapply(makeGRangesFromDataFrame, keep.extra.columns=TRUE)
+    grl_in = sample %>%                                             
+             group_split(cell_type) %>%                             
+             lapply(makeGRangesFromDataFrame, keep.extra.columns=TRUE)
 		grl = GRangesList(grl_in)
 		count_overlaps = lapply(grl, function(x) countOverlaps (interval.ranges, x))
 		cell_types = unlist(lapply(grl, function(x) unique(x$cell_type)))
