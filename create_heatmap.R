3cb8fd05c490e176c146c9f1819ce7f085d3c90f
Mohamad Bairakdar <mdanb@umich.edu>
2022-06-23 16:15:50 -0400

    add fragment numbersto heatmaps

--- create_heatmap.R
+++ create_heatmap.R
@@ -16,6 +16,7 @@ if (length(args) != 1) {
 } 
 
 dir.create("figures") 
+dir.create("processed_data")
 
 get_tissue_name <- function(filename) {
   if (grepl("frontal_cortex", file)) {
@@ -31,22 +32,26 @@ get_tissue_name <- function(filename) {
   return(tissue_name)
 }
 
-save_heatmap_file <- function(path, df) {
+save_heatmap_file <- function(path, df, fragment_per_row) {
   pdf(path)
+  row_ha = rowAnnotation(fragment_number = anno_text(fragment_per_row))
   h = Heatmap(df, row_names_gp = gpar(fontsize = 8),
-              column_names_gp = gpar(fontsize = 6))
+              column_names_gp = gpar(fontsize = 6), 
+              left_annotation = row_ha,
+              show_column_dend = FALSE, show_row_dend = FALSE)
   print(h)
   dev.off()
 }
 
-create_per_tissue_heatmap <- function(cor_df, n, i) {
+create_per_tissue_heatmap <- function(cor_df, n, i, frag_df) {
   tissue = n[i]
   tissue = gsub(" ", "_", tolower(tissue), )
   heatmap_filename = tissue
   correlations = cor_df[[i]]
+  fragments_per_row = frag_df[[i]]
   
   path = paste("figures/per_tissue_heatmaps/", heatmap_filename, ".pdf", sep="")
-  save_heatmap_file(path, correlations) 
+  save_heatmap_file(path, correlations, fragments_per_row) 
   
   correlations_rowwise = t(scale(t(correlations)))
   correlations_columnwise = scale(correlations)
@@ -59,8 +64,8 @@ create_per_tissue_heatmap <- function(cor_df, n, i) {
   column_path = paste("figures/per_tissue_heatmaps/", column_heatmap_filename, 
                       ".pdf", sep="")
   
-  save_heatmap_file(row_path, correlations_rowwise)
-  save_heatmap_file(column_path, correlations_columnwise)
+  save_heatmap_file(row_path, correlations_rowwise, fragments_per_row)
+  save_heatmap_file(column_path, correlations_columnwise, fragments_per_row)
 }
 # get all mutations, without distinctions e.g clonal vs subclonal 
 mut = mut[, 1:37]
@@ -77,6 +82,7 @@ for (cancer_type in colnames(mut)) {
 mut_count_data = as.data.frame(do.call(cbind, cancer_types_mut_data))
 colnames(mut_count_data) = colnames(mut)
 rownames(mut_count_data) = names(interval.ranges)
+write.csv(mut_count_data, "processed_data/mut_count_data.csv")
 
 combined_filepath = paste("count_overlap_data/processed_count_overlaps/", 
                           "count_filter_",
@@ -93,6 +99,7 @@ unsquashed_tissue_names_filepath = paste("count_overlap_data/processed_count_ove
                                      CELL_NUMBER_FILTER, 
                                      "_unsquashed_tissue_names.rds", sep="")
 
+#### One heatmap, all tissues ####
 pattern = paste("count_filter_", CELL_NUMBER_FILTER, "_", sep="")
 if (!file.exists(combined_filepath) || !file.exists(unsquashed_overlaps_filepath)) {
   combined_count_overlaps = data.frame()
@@ -141,20 +148,8 @@ combined_count_overlaps = t(readRDS(combined_filepath))
 
 correlations = cor(combined_count_overlaps, mut_count_data, use="complete.obs")
 correlations = t(scale(t(correlations)))
-# correlations = scale(correlations)
 
-#write.csv(correlations, "all_corrs.csv")
-
-#png(file="/home/mdanb/research/mount_sinai/bing_ren/corrs.png")
-#pdf ("/home/mdanb/research/mount_sinai/bing_ren/corrs.pdf", width = 10, height = 30)
-#h = Heatmap(correlations,
-#            row_names_gp = gpar(fontsize = 6),
-#            column_names_gp = gpar(fontsize = 6),height=18,
-#)
-#print(h)
-#dev.off()
-
-
+#### Tissue specific heatmaps ####
 tissue_specific_counts_path = "count_overlap_data/processed_count_overlaps/tissue_specific_counts_list.rds"
 tissue_specific_counts = list()
 
@@ -196,14 +191,16 @@ tissue_specific_counts = readRDS("count_overlap_data/processed_count_overlaps/ti
 tissue_specific_counts = lapply(tissue_specific_counts, t)
 per_tissue_cor = lapply(tissue_specific_counts, cor, mut_count_data, 
                         use="complete.obs")
+per_tissue_fragment_counts = lapply(tissue_specific_counts, colSums)
 
 dir.create("figures/per_tissue_heatmaps")
 lapply(seq_along(per_tissue_cor), create_per_tissue_heatmap, 
        cor_df=per_tissue_cor, 
-       n=names(per_tissue_cor))
+       n=names(per_tissue_cor),
+       frag_df=per_tissue_fragment_counts)
 
 
-#### ####
+#### Avg correlation vs number of mutations ####
 correlations = cor(combined_count_overlaps, mut_count_data, use="complete.obs")
 muts_per_cancer_type = apply(mut, 2, sum)
 avg_corrs_across_cell_types = apply(correlations, 2, mean)
@@ -216,33 +213,7 @@ ggplot(df, aes(x=muts_per_cancer_type, y=avg_corrs_across_cell_types)) +
 
 cor(muts_per_cancer_type, avg_corrs_across_cell_types)
 
-#### ####
-# obtain_x_y_group_triplets <- function(df, n, i, cell_type, 
-#                                       combined_count_overlaps) {
-#   df = df[[i]]
-#   tissue = n[i]
-#   pattern = paste(tissue, ".*", cell_type, sep="")
-#   x = sum(combined_count_overlaps[, grepl(pattern=pattern, 
-#                                   colnames(combined_count_overlaps))])
-#   relevant_cells = df[grepl(cell_type, rownames(df), ignore.case = T), ]
-#   if (!is.null(dim(relevant_cells))) {
-#     y = apply(relevant_cells, 1,  min)
-#   }
-#   else {
-#     y = min(relevant_cells)
-#   }
-#   return(data.frame(cell_num=rep(x, length(y)), corr=y, tissue=tissue,
-#                     row.names=NULL))
-# }
-
-# fibroblast_min_corrs_per_tissue = 
-#                    lapply(seq_along(per_tissue_cor), obtain_x_y_group_triplets,
-#                    df = per_tissue_cor, n = names(per_tissue_cor), 
-#                    cell_type ='Fibroblast', 
-#                    combined_count_overlaps = combined_count_overlaps) %>%
-#                    bind_rows()
-
-# cor(fibroblast_min_corrs_per_tissue['cell_num'], fibroblast_min_corrs_per_tissue['corr'])
+#### Min correlation vs number of fragments ####
 cell_type = 'Fibroblast'
 total_fragments_per_sample = rowSums(unsquashed_count_overlaps)
 unsquashed_correlations = cor(t(unsquashed_count_overlaps), mut_count_data, 
@@ -253,47 +224,69 @@ df = data.frame(fragments=total_fragments_per_sample, correlation=min_corr_per_s
 df = df[grepl(cell_type, rownames(df), ignore.case = T), ]
 ggplot(df, aes(x=fragments, y=correlation, color=tissue)) +
   geom_point() 
+# stat_cor(method = "pearson",label.x = 5000000, label.y = -1.5)
 
 cor(df[,'fragments'], df[, 'correlation'])
 
-#+
- # stat_cor(method = "pearson",label.x = 5000000, label.y = -1.5)
+#### Polak Count overlaps ####
+polak_combined_count_overlaps = data.frame()
+if (!file.exists("polak_count_overlap_data/processed_count_overlaps/polak_combined_count_overlaps.rds")) {
+  polak_combined_count_overlaps = data.frame()
+  for (file in list.files("polak_count_overlap_data", full.names=TRUE, pattern=
+                          "count_overlaps_")) {
+    if (grepl("uniques", file)) {
+      next
+    }
+    else {
+      tissue_or_cell_name = unlist(strsplit(file, ".", fixed = TRUE))[2]
+      
+      if (grepl("Left", tissue_or_cell_name) || 
+          grepl("Right", tissue_or_cell_name)) {
+        tissue_or_cell_name = as.list(unlist(strsplit(tissue_or_cell_name, "_")))
+        tissue_or_cell_name = tissue_or_cell_name[1:length(tissue_or_cell_name)-1]
+        tissue_or_cell_name = str_to_title(paste(unlist(tissue_or_cell_name),
+                           collapse=" "))
+      }
+      else {
+        tissue_or_cell_name = str_to_title(paste(unlist(strsplit(tissue_or_cell_name, "_")),
+                                       collapse=" "))
+      }
+    }
+    count_overlaps = readRDS(file)
+    count_overlaps = t(as.data.frame(count_overlaps))
+    row.names(count_overlaps) = tissue_or_cell_name
+    count_overlaps_exists = rownames(count_overlaps) %in%
+                            rownames(polak_combined_count_overlaps)
+    tryCatch(
+          if (count_overlaps_exists) {
+            idx = match(rownames(count_overlaps), rownames(polak_combined_count_overlaps))
+            polak_combined_count_overlaps[idx, ] = polak_combined_count_overlaps[idx, ] +
+              count_overlaps[1, ]
+          }
+          else {
+            polak_combined_count_overlaps = rbind(polak_combined_count_overlaps, 
+                                                  count_overlaps)
+          },
+          error = function(err) {
+            print(paste(file, "has no count overlaps"), sep=" ")
+     })
+  }
+  saveRDS(polak_combined_count_overlaps, 
+          "polak_count_overlap_data/processed_count_overlaps/polak_combined_count_overlaps.rds")
+}
 
-#### ####
+polak_combined_filepath = 
+  "polak_count_overlap_data/processed_count_overlaps/polak_combined_count_overlaps.rds"
 
+polak_combined_count_overlaps = t(readRDS(polak_combined_filepath))
 
-# polak_combined_count_overlaps = data.frame()
-# if (!file.exists("polak_count_overlap_data/polak_combined_count_overlaps.rds")) {
-#   polak_combined_count_overlaps = data.frame()
-#   for (file in list.files("polak_count_overlap_data", full.names=TRUE)) {
-#     if (grepl("IMR90", file)) {
-#       tissue_name = "IMR90"
-#     }
-#     else {
-#       tissue_name = unlist(strsplit(file, ".", fixed = TRUE))[2]
-#       tissue_name = str_to_title(paste(unlist(strsplit(mystr, "_")), 
-#                                        collapse=" "))
-#     }
-#     count_overlaps = readRDS(file)
-#     count_overlaps = as.data.frame(do.call(rbind, count_overlaps),
-#                                    row.names = paste(tissue_name,
-#                                                      names(count_overlaps)))
-#     for (i in 1:nrow(count_overlaps)) {
-#       count_overlaps_exists = rownames(count_overlaps)[i] %in%
-#         rownames(combined_count_overlaps)
-#       tryCatch(
-#         if (count_overlaps_exists) {
-#           idx = match(rownames(count_overlaps)[i], rownames(combined_count_overlaps))
-#           combined_count_overlaps[idx, ] = combined_count_overlaps[idx, ] +
-#             count_overlaps[i, ]
-#         }
-#         else {
-#           combined_count_overlaps = rbind(combined_count_overlaps, count_overlaps[i, ])
-#         },
-#         error = function(err) {
-#           print(paste(file, "has no count overlaps"), sep=" ")
-#         })
-#     }
-#   }
-#   saveRDS(combined_count_overlaps, "count_overlap_data/combined_count_overlaps.rds")
-# }
+correlations = cor(polak_combined_count_overlaps, mut_count_data, use="complete.obs")
+correlations = scale(correlations)
+
+pdf ("/home/mdanb/research/mount_sinai/bing_ren/corrs.pdf", width = 10, height = 30)
+h = Heatmap(correlations,
+           row_names_gp = gpar(fontsize = 6),
+           column_names_gp = gpar(fontsize = 6),height=18,
+)
+print(h)
+dev.off()
