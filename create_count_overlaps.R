ea42141331e78d550fe31726a85c04935d12e26f
Mohamad Bairakdar <mdanb@umich.edu>
2022-06-24 13:28:32 -0400

    parallelize create_count_overlaps.R

--- create_count_overlaps.R
+++ create_count_overlaps.R
@@ -5,6 +5,7 @@ library(data.table)
 library(dplyr)                                                                  
 library(tibble)                                                                 
 library(exomeCopy)
+library(parallel)
 
 get_sample_name <- function(file) {
   if (grepl("frontal_cortex", file)) {
@@ -29,7 +30,7 @@ filter_sample_by_cell_number <- function(sample, cell_number_filter) {
   return(sample)
 }
 
-get_count_overlaps <- function(sample, interval_ranges) {
+compute_count_overlaps <- function(sample, interval_ranges) {
   grl_in = sample %>%                                             
     group_split(cell_type) %>%                             
     lapply(makeGRangesFromDataFrame, keep.extra.columns=TRUE)
@@ -53,26 +54,8 @@ get_sample_cell_types <- function(sample, sample_name, metadata) {
   return(as.tibble(sample))
 }
 
-
-args = commandArgs(trailingOnly=TRUE)
-if (length(args) != 1) {
-  stop("One argument must be supplied", call.=FALSE)
-} else {
-  CELL_NUMBER_FILTER = as.integer(args[1])
-} 
-
-load('raw_dir/mutation_data/hg19.1Mb.ranges.Polak.Nature2015.RData')
-
-interval.ranges
-subdivided_interval_ranges = subdivideGRanges(interval.ranges, subsize = 200000)
-dir.create("count_overlap_data")                                                
-
-metadata = read.table("raw_dir/metadata/GSE184462_metadata.tsv", sep="\t", 
-                      header=TRUE)  
-
-for (file in setdiff(list.files("raw_dir/bed_files/"), 
-                     list.dirs("raw_dir/bed_files", recursive = FALSE, 
-                               full.names = FALSE))) {                                    
+create_count_overlaps_file <- function(file, cell_number_filter, metadata,
+                                       interval_ranges) {
   filename = paste("count_filter", CELL_NUMBER_FILTER,  
                    "count_overlaps", paste(file_path_sans_ext(file, TRUE),
                                            "rds", sep="."), sep="_")
@@ -87,11 +70,13 @@ for (file in setdiff(list.files("raw_dir/bed_files/"),
     sample <- get_sample_cell_types(sample, sample_name, metadata)
     sample <- filter_sample_by_cell_number(sample, CELL_NUMBER_FILTER)
     if (!file.exists(filename)) {
-      count_overlaps <- get_count_overlaps(sample, interval.ranges)
+      count_overlaps <- compute_count_overlaps(sample, interval_ranges)
       saveRDS(count_overlaps, paste("count_overlap_data", filename, sep="/"))
     }
     if (!file.exists(subdivided_filename)) {
-      count_overlaps_subdivided <- get_count_overlaps(sample, 
+      subdivided_interval_ranges = subdivideGRanges(interval.ranges, 
+                                                    subsize = 200000)
+      count_overlaps_subdivided <- compute_count_overlaps(sample, 
                                                      subdivided_interval_ranges)
       saveRDS(count_overlaps_subdivided, paste("count_overlap_data", 
                                                subdivided_filename, 
@@ -99,3 +84,27 @@ for (file in setdiff(list.files("raw_dir/bed_files/"),
     }
   }
 }
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) != 1) {
+  stop("One argument must be supplied", call.=FALSE)
+} else {
+  CELL_NUMBER_FILTER = as.integer(args[1])
+} 
+
+metadata = read.table("raw_dir/metadata/GSE184462_metadata.tsv", sep="\t", 
+                      header=TRUE)  
+load('raw_dir/mutation_data/hg19.1Mb.ranges.Polak.Nature2015.RData')
+interval.ranges
+
+dir.create("count_overlap_data")                                                
+
+files = setdiff(list.files("raw_dir/bed_files/"), 
+                list.dirs("raw_dir/bed_files", recursive = FALSE, 
+                          full.names = FALSE))
+
+mclapply(files, create_count_overlaps_file, 
+                cell_number_filter=CELL_NUMBER_FILTER,
+                metadata=metadata,
+                interval_ranges=interval.ranges,
+                mc.cores = 6)
