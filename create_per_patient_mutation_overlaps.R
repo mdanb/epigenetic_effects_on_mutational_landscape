82dac0e5a7247daa02b6e338f7e6d93478a47392
Mohamad Bairakdar <mdanb@umich.edu>
2022-07-07 18:19:24 -0400

    add option to provide args

--- create_per_patient_mutation_overlaps.R
+++ create_per_patient_mutation_overlaps.R
@@ -1,41 +1,69 @@
 library(GenomicRanges)
+library(optparse)
 
-Lung_AdenoCA = list.files("processed_data/per_patient_mutations/Lung-AdenoCA", 
-                           full.names = TRUE)
-Skin_Melanoma = list.files("processed_data/per_patient_mutations/Skin-Melanoma", 
-                          full.names = TRUE)
 load('raw_dir/mutation_data/hg19.1Mb.ranges.Polak.Nature2015.RData')
 
+option_list <- list( 
+  make_option(c("--cancer_types", type="str"))
+)
+
+cancer_types = parse_args(OptionParser(option_list=option_list), 
+                args = c("--cancer_types", "Skin-Melanoma"))
+
+cancer_types = unlist(strsplit(cancer_types$cancer_types, split = ","))
+
 count_overlaps_and_save <- function(filepath, per_patient_mutations_dir) {
-  mutations = read.csv(filepath)
-  if (!grepl("chr", mutations["chromosome"])) {
-    seqnames = paste(rep("chr", dim(mutations["chromosome"])[1]), 
-                     mutations[["chromosome"]], sep="")
-  }
-  else {
-    seqnames = mutations[["chromosome"]]
-  }
-  if ("chrMT" %in% mutations[["chromosome"]]) {
-    idx = match("chrMT", mutations[["chromosome"]])
-    mutations = mutations[-idx, ]
-    seqnames = seqnames[-idx]
-  }
-  irange_patient_mutation = IRanges(start=
-                                      as.vector(unlist(mutations["chromosome_start"])), 
-                                    end=
-                                      as.vector(unlist(mutations["chromosome_end"])))
-  grange_patient_mutation = GRanges(seqnames=seqnames,
-                                    ranges=irange_patient_mutation)
-  count_overlaps = as.data.frame(countOverlaps(interval.ranges, 
-                                               grange_patient_mutation))
-  colnames(count_overlaps) <- "num_mutations"
   filename = unlist(strsplit(filepath, "/"))
   filename = paste("binned_mutations", filename[length(filename)], sep="_")
-  write.csv(as.data.frame(count_overlaps), paste(per_patient_mutations_dir, 
-                                                 filename, sep="/"))
+  mutations = read.csv(filepath)
+  if (!file.exists(paste(per_patient_mutations_dir, 
+                         filename, sep="/"))) {
+    if (!grepl("chr", mutations["chromosome"])) {
+      seqnames = paste(rep("chr", dim(mutations["chromosome"])[1]), 
+                       mutations[["chromosome"]], sep="")
+    }
+    else {
+      seqnames = mutations[["chromosome"]]
+    }
+    if ("chrMT" %in% seqnames) {
+      idx = match("chrMT", seqnames)
+      mutations = mutations[-idx, ]
+      seqnames = seqnames[-idx]
+    }
+    irange_patient_mutation = IRanges(start=
+                                        as.vector(unlist(mutations["chromosome_start"])), 
+                                      end=
+                                        as.vector(unlist(mutations["chromosome_end"])))
+    grange_patient_mutation = GRanges(seqnames=seqnames,
+                                      ranges=irange_patient_mutation)
+    count_overlaps = as.data.frame(countOverlaps(interval.ranges, 
+                                                 grange_patient_mutation))
+    colnames(count_overlaps) <- "num_mutations"
+    write.csv(as.data.frame(count_overlaps), paste(per_patient_mutations_dir, 
+                                                   filename, sep="/"))
+  }
 }
 
-lapply(Lung_AdenoCA, count_overlaps_and_save, 
-       "processed_data/per_patient_mutations/Lung-AdenoCA")
-lapply(Skin_Melanoma, count_overlaps_and_save, 
-       "processed_data/per_patient_mutations/Skin-Melanoma")
+parallel_count_overlaps_and_save <- function(cancer_type) {
+  filespath = paste("processed_data/per_patient_mutations", cancer_type, sep="/")
+  files = grep("^((?!binned).)*$", list.files(filespath, full.names = TRUE),
+               perl = TRUE, value = TRUE)
+  lapply(files, count_overlaps_and_save, filespath)
+}
+# Lung_AdenoCA = list.files("processed_data/per_patient_mutations/Lung-AdenoCA", 
+#                           full.names = TRUE)
+# Skin_Melanoma = list.files("processed_data/per_patient_mutations/Skin-Melanoma", 
+#                            full.names = TRUE)
+
+lapply(cancer_types, parallel_count_overlaps_and_save)
+
+# for (cancer_type in cancer_types) {
+#   filespath = paste("processed_data/per_patient_mutations", cancer_type, sep="/")
+#   files = list.files(filespath, full.names = TRUE)
+#   lapply(files, count_overlaps_and_save, filespath)
+# }
+
+# lapply(Lung_AdenoCA, count_overlaps_and_save, 
+#        "processed_data/per_patient_mutations/Lung-AdenoCA")
+# lapply(Skin_Melanoma, count_overlaps_and_save, 
+#        "processed_data/per_patient_mutations/Skin-Melanoma")
--- create_per_patient_mutations.py
+++ create_per_patient_mutations.py
@@ -1,13 +1,32 @@
 import pandas as pd
 import os
+import json
+import argparse
 
-def load_and_clean_mutations(filepath, *args):
-    per_patient_mutations = pd.read_csv(filepath, sep="\t", usecols=["icgc_mutation_id", "icgc_donor_id",
-                                                                     "chromosome", "chromosome_start",
-                                                                     "chromosome_end"])
-    for file in args:
-        df = pd.read_csv(file, sep="\t")
-        per_patient_mutations = pd.concat((per_patient_mutations, df), axis=0)
+parser = argparse.ArgumentParser()
+parser.add_argument("--cancer_types", nargs="+", type=str,
+                    help='which cancer types to create mutation files for', required=True)
+config = parser.parse_args()
+cancer_types = config.cancer_types
+
+def load_filenames_per_cancer(cancer_type):
+    f = open(f"raw_dir/mutation_data/per_cancer_icgc/{cancer_type}/{cancer_type}.json")
+    data = json.load(f)
+    return data["files"]
+
+def all_patients_are_processed(filepath, cancer_type):
+    patients = pd.unique(pd.read_csv(filepath,
+                         sep="\t", usecols=["icgc_donor_id"],
+                         dtype={"icgc_donor_id":str})["icgc_donor_id"])
+    for patient in patients:
+        if (not os.path.exists(f"processed_data/per_patient_mutations/{cancer_type}/{patient}.csv")):
+            return False
+    return True
+
+def load_and_clean_mutations(file):
+    per_patient_mutations = pd.read_csv(file, sep="\t", usecols=["icgc_mutation_id", "icgc_donor_id",
+                                                                 "chromosome", "chromosome_start",
+                                                                 "chromosome_end"])
     unique_mutations = per_patient_mutations[["icgc_donor_id", "icgc_mutation_id"]].drop_duplicates()
 
     chromosome_start_end = per_patient_mutations[["icgc_mutation_id",
@@ -34,10 +53,14 @@ def create_mutations_per_patient_files(mutations, cancer_type):
 #                                                             "simple_somatic_mutation.open.LUAD-US.tsv")
 # create_mutations_per_patient_files(per_patient_mutations_lung_adeno, "Lung-AdenoCA")
 
-per_patient_mutations_skin_melanoma = load_and_clean_mutations("raw_dir/mutation_data/per_cancer_icgc/Skin-Melanoma/"
-                                                               "melanoma_combined_mutations.tsv")
+for cancer_type in cancer_types:
+    files = load_filenames_per_cancer(cancer_type)
+    for file in files:
+        if not all_patients_are_processed(file, cancer_type):
+            per_patient_mutations = load_and_clean_mutations(file)
+            create_mutations_per_patient_files(per_patient_mutations, cancer_type)
+
 # per_patient_mutations_skin_melanoma = load_and_clean_mutations("raw_dir/mutation_data/per_cancer_icgc/Skin-Melanoma/"
 #                                                                 "simple_somatic_mutation.open.MELA-AU.tsv",
 #                                                                "raw_dir/mutation_data/per_cancer_icgc/Skin-Melanoma/"
 #                                                                 "simple_somatic_mutation.open.SKCM-US.tsv")
-create_mutations_per_patient_files(per_patient_mutations_skin_melanoma, "Skin-Melanoma")
