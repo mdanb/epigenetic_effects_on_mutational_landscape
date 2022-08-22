3a1c9a44d587ab917d33b47752ce2add3eefe091
Mohamad Bairakdar <mdanb@umich.edu>
2022-08-22 13:29:57 -0400

    readd mel

--- create_count_overlaps.R
+++ create_count_overlaps.R
@@ -837,38 +837,38 @@ shendure_lung_files  = setdiff(list.files("raw_dir/bed_files/JShendure_scATAC/",
 #                                                c("Distal Lung AT2"))
 
 #### Melanoma ####
-# bing_ren_skin_files = setdiff(list.files("raw_dir/bed_files/",
-#                                          pattern=".*skin_SM*"),
-#                               list.dirs("raw_dir/bed_files", recursive = FALSE,
-#                                         full.names = FALSE))
-# 
-# top_tsse_fragment_count_range = c(1000, 2983, 8901, 26558, 79237, 236409,
-#                                   705340, 2104417)
-# skin_cell_types = c("Melanocyte",
-#                     "Fibroblast (Epithelial)")
-# create_tsse_filtered_count_overlaps_per_tissue(bing_ren_skin_files,
-#                                               CELL_NUMBER_FILTER,
-#                                               metadata,
-#                                               interval.ranges,
-#                                               ch,
-#                                               top_tsse_fragment_count_range,
-#                                               skin_cell_types,
-#                                               "bing_ren")
-# 
-# 
-# bing_ren_skin_sun_exposed_files = setdiff(list.files("raw_dir/bed_files/",
-#                                               pattern=".*skin_sun_exposed.*"),
-#                               list.dirs("raw_dir/bed_files", recursive = FALSE,
-#                                         full.names = FALSE))
-# 
-# create_tsse_filtered_count_overlaps_per_tissue(bing_ren_skin_sun_exposed_files,
-#                                                CELL_NUMBER_FILTER,
-#                                                metadata,
-#                                                interval.ranges,
-#                                                ch,
-#                                                top_tsse_fragment_count_range,
-#                                                skin_cell_types,
-#                                                "bing_ren")
+bing_ren_skin_files = setdiff(list.files("raw_dir/bed_files/",
+                                         pattern=".*skin_SM*"),
+                              list.dirs("raw_dir/bed_files", recursive = FALSE,
+                                        full.names = FALSE))
+
+top_tsse_fragment_count_range = c(1000, 2983, 8901, 26558, 79237, 236409,
+                                  705340, 2104417)
+skin_cell_types = c("Melanocyte",
+                    "Fibroblast (Epithelial)")
+create_tsse_filtered_count_overlaps_per_tissue(bing_ren_skin_files,
+                                              CELL_NUMBER_FILTER,
+                                              metadata,
+                                              interval.ranges,
+                                              ch,
+                                              top_tsse_fragment_count_range,
+                                              skin_cell_types,
+                                              "bing_ren")
+
+
+bing_ren_skin_sun_exposed_files = setdiff(list.files("raw_dir/bed_files/",
+                                              pattern=".*skin_sun_exposed.*"),
+                              list.dirs("raw_dir/bed_files", recursive = FALSE,
+                                        full.names = FALSE))
+
+create_tsse_filtered_count_overlaps_per_tissue(bing_ren_skin_sun_exposed_files,
+                                               CELL_NUMBER_FILTER,
+                                               metadata,
+                                               interval.ranges,
+                                               ch,
+                                               top_tsse_fragment_count_range,
+                                               skin_cell_types,
+                                               "bing_ren")
 
 #### ColoRect.AdenoCA ####
 bing_ren_colon_files = setdiff(list.files("raw_dir/bed_files/",
