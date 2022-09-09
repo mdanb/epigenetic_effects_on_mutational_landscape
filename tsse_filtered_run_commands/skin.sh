1abcfd1717d9ed04904f58819d792609208d8349
bgiotti <brunogiotti@gmail.com>
2022-09-09 02:32:58 +0000

    add tsse filtered run commands

---
+++ tsse_filtered_run_commands/skin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript ../create_tsse_filtered_count_overlaps.R --top_tsse_fragment_count_range="1000,10000,50000,100000,150000,250000,300000,400000,500000,600000" --dataset="bing_ren" --cell_types="Melanocyte,Fibroblast (Epithelial)" --files_pattern="skin_SM"
