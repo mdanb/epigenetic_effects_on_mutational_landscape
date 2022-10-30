fa7796257eeafd12ab68591cdf67d91a4cb23885
bgiotti <brunogiotti@gmail.com>
2022-10-30 14:22:13 +0000

    all tsse filtered commands

--- tsse_filtered_run_commands/lung_bingren.sh
+++ tsse_filtered_run_commands/lung_bingren.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript ../create_tsse_filtered_count_overlaps.R --top_tsse_fragment_count_range="1000,10000,50000,100000,150000,250000,300000,400000,500000,600000" --dataset="bing_ren" --cell_types="Alveolar Type 2 (AT2) Cell" --files_pattern="lung_SM" --cores=1
+Rscript ../create_tsse_filtered_count_overlaps.R --top_tsse_fragment_count_range="1000,10000,50000,100000,150000,250000,300000,400000,500000,1000000,1500000,2000000,5000000,15000000,20000000" --dataset="bing_ren" --cell_types="Alveolar Type 2 (AT2) Cell,Club Cell" --files_pattern="lung_SM" --cores=1
--- tsse_filtered_run_commands/stomach_shendure.sh
+++ tsse_filtered_run_commands/stomach_shendure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript ../create_tsse_filtered_count_overlaps.R --top_tsse_fragment_count_range="14388809" --dataset="shendure" --cell_types="Goblet cells" --files_pattern="stomach" --cores=1
+Rscript ../create_tsse_filtered_count_overlaps.R --top_tsse_fragment_count_range="1000,10000,50000,100000,150000,250000,300000,400000,500000,1000000,1500000,2000000,5000000,10000000,15000000,20000000" --dataset="shendure" --cell_types="Goblet cells,Stromal cells" --files_pattern="stomach" --cores=1
