ca6a4b69d348ca3e78c38b1d2981579c43dfc8fd
Mohamad Bairakdar <mdanb@umich.edu>
2022-11-23 21:56:59 -0500

    make tissue separable

--- utils.R
+++ utils.R
@@ -33,8 +33,10 @@ get_tissue_name <- function(filename) {
     tissue_name = unlist(strsplit(str_extract(filename, 
                                               "count_overlaps_.*[0-9]_.*_SM"), 
                                   "_"))
-    tissue_name = str_to_title(paste(tissue_name[4:(length(tissue_name)-1)], 
-                                     collapse=" "))
+    tissue_name = paste(tissue_name[4:(length(tissue_name)-1)], 
+                                     collapse="_")
+    # tissue_name = str_to_title(paste(tissue_name[4:(length(tissue_name)-1)], 
+    #                                  collapse=" "))
   }
   return(tissue_name)
 }
@@ -43,7 +45,8 @@ get_tissue_name_shendure <- function(filename) {
   tissue_name = unlist(strsplit(str_extract(filename, 
                                             "sample_.*[.]fragments"), "_"))[3]
   tissue_name = unlist(strsplit(tissue_name, "[.]"))[1]
-  tissue_name = str_to_title(tissue_name)
+  tissue_name = paste(tissue_name, collapse="_")
+  # tissue_name = str_to_title(tissue_name)
   return(tissue_name)
 }
 
@@ -52,8 +55,9 @@ get_tissue_name_tsankov <- function(filename) {
                                             "count_overlaps_.*_patient"), 
                                 "_"))
   tissue_name = tissue_name[3:(length(tissue_name) - 1)]
-  tissue_name = paste(tissue_name, collapse=" ")
-  tissue_name = str_to_title(tissue_name)
+  tissue_name = paste(tissue_name, collapse="_")
+  # tissue_name = paste(tissue_name, collapse=" ")
+  # tissue_name = str_to_title(tissue_name)
   return(tissue_name)
 }
 
