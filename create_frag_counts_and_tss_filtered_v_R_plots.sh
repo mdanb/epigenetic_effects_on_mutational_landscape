063e3a2fa58539933d06ac4269bba97a7be6499d
Mohamad Bairakdar <mdanb@umich.edu>
2022-11-27 09:46:40 -0500

    f

--- create_frag_counts_and_tss_filtered_v_R_plots.sh
+++ create_frag_counts_and_tss_filtered_v_R_plots.sh
@@ -220,6 +220,18 @@ fi
 #    --plot_x_ticks "1000,10000,50000,100000,150000,250000,300000,400000,500000,1000000,1500000,2000000,5000000,10000000,15000000,20000000"
 #fi
 #
+
+if ! [ -f "figures/lung_scc_BR_only_with_bottom_feats_num_frags_vs_correlation.png" ]; then               
+    Rscript create_frag_counts_and_tss_filtered_v_R_plots.R \                  
+    --cancer_type "Lung.SCC" \                                                 
+    --boxplot_cell_types "Esophagus Mucosa Esophageal Epithelial Cell (BR),Artery Aorta Smooth Muscle (General) (BR),Lung Club Cell (BR),Small Intestine T Lymphocyte 1 (CD8+) (BR),Vagina Smooth Muscle (General) (BR),Colon Sigmoid Fibroblast (Gastrointestinal) (BR),Esophagus Ge Junction Smooth Muscle (GE Junction) (BR),Artery Aorta Fibroblast (General) (BR),Colon Transverse Macrophage (General) (BR),Adipose Omentum Fibroblast (General) (BR)" \
+    --tsse_filtered_cell_types "Esophageal Epithelial Cell,Smooth Muscle (General),Club Cell,T Lymphocyte 1 (CD8+),Smooth Muscle (General),Fibroblast (Gastrointestinal),Smooth Muscle (GE Junction),Fibroblast (General),Macrophage (General),Fibroblast (General)" \
+    --tissue_for_tsse_filtered_cell_types "Bing Ren-Esophagus Mucosa,Bing Ren-Artery Aorta,Bing Ren-Lung,Bing Ren-Small Intestine,Bing Ren-Vagina,Bing Ren-Colon Sigmoid,Bing Ren-Esophagus Ge Junction,Bing Ren-Artery Aorta,Bing Ren-Colon Transverse,Bing Ren-Adipose Omentum" \
+    --plot_filename "lung_scc_BR_only_with_bottom_feats_num_frags_vs_correlation.png" \          
+    --plot_x_ticks "1000,10000,50000,100000,150000,250000,300000,400000,500000,1000000,1500000,2000000,5000000,10000000"
+fi                                                                             
+
+
 ## Liver HCC
 #if ! [ -f "figures/liver_hcc_num_frags_vs_correlation.png" ]; then
 #	Rscript create_frag_counts_and_tss_filtered_v_R_plots.R \
