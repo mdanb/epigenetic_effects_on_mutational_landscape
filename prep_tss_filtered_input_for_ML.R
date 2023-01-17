fdd455ceff5f2d055e854507519f539de82839bd
Mohamad Bairakdar <mdanb@umich.edu>
2023-01-16 21:15:38 -0500

    add br shendure tsankov

--- prep_tss_filtered_input_for_ML.R
+++ prep_tss_filtered_input_for_ML.R
@@ -2,14 +2,20 @@ library(optparse)
 library(dplyr)
 
 option_list <- list( 
-  make_option("--fragment_count_range", type="character")
+  make_option("--fragment_count_range", type="character"),
+  make_option("--bing_ren", type="logical", default=F),
+  make_option("--shendure", type="logical", default=F),                         
+  make_option("--tsankov", type="logical", default=F)
 )
 
 args = parse_args(OptionParser(option_list=option_list))
-# args = parse_args(OptionParser(option_list=option_list), args=
-#                    c("--fragment_count_range=100000,150000,200000"))
+#args = parse_args(OptionParser(option_list=option_list), args=
+#                   c("--fragment_count_range=50000,100000,300000"))
 
 fragment_count_range = unlist(strsplit(args$fragment_count_range, ","))
+bing_ren = args$bing_ren
+shendure = args$shendure
+tsankov = args$tsankov
 
 root = "/ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/processed_data/count_overlap_data/tsse_filtered"
 bingren_path = paste(root, "bing_ren", sep="/")
@@ -23,9 +29,11 @@ combined_tss_filtered_cells <- function(fragment_count_range, tss_path) {
     combined_df_list = list()
     for (dir in setdiff(list.dirs(tss_path, recursive = F, full.names = F),
                         "combined")) {
-      df = readRDS(list.files(paste(tss_path, dir, sep="/"), 
-                              pattern=paste0(fragment_count, ".rds"), 
-                              full.names = T))
+      df = readRDS(paste(paste(tss_path, dir, sep="/"), 
+                         paste0("count_overlaps_frag_count_filter_",
+                                fragment_count, 
+                                ".rds"), 
+                   sep="/"))
       if (nrow(df) == 0) {
         print(paste(dir, "has no count overlaps for", fragment_count, 
                     "fragments"), sep=" ")
@@ -42,7 +50,14 @@ combined_tss_filtered_cells <- function(fragment_count_range, tss_path) {
   }
 }
 
-combined_tss_filtered_cells(fragment_count_range, bingren_path)
-combined_tss_filtered_cells(fragment_count_range, shendure_path)
-combined_tss_filtered_cells(fragment_count_range, tsankov_path)
+if (bing_ren) {
+	combined_tss_filtered_cells(fragment_count_range, bingren_path)
+}
 
+if (shendure) {
+	combined_tss_filtered_cells(fragment_count_range, shendure_path)
+}
+
+if (tsankov) {
+	combined_tss_filtered_cells(fragment_count_range, tsankov_path)
+}
