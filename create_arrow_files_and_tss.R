da3b7ca8fc5f8b144cb00c85f4b575a066f76948
Mohamad Bairakdar <mdanb@umich.edu>
2023-01-17 19:42:58 -0500

    f

--- create_arrow_files_and_tss.R
+++ create_arrow_files_and_tss.R
@@ -26,7 +26,7 @@ create_arrow_files_and_tss <- function(genome_version, fragment_paths,
                                    force = F)
     archp = ArchRProject(ArrowFiles = arrow_files, 
                          outputDirectory = output_dir,
-                         copyArrows = FALSE)
+                         copyArrows = F)
     saveArchRProject(archp)
     write.csv(archp@cellColMetadata, paste(output_dir, "metadata.csv", sep="/"))
   }
@@ -34,8 +34,16 @@ create_arrow_files_and_tss <- function(genome_version, fragment_paths,
 
 if (dataset == "bing_ren") {
   output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files"
-  create_arrow_files_and_tss("hg38", list.files(output_dir), output_dir)
+  create_arrow_files_and_tss("hg38", 
+                             setdiff(list.files(output_dir, full.names=T),
+                                     list.dirs(output_dir, recursive = F, 
+                                               full.names = T)), 
+                             output_dir)
 } else if (dataset == "shendure") {
   output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/JShendure_scATAC"
-  create_arrow_files_and_tss("hg19", list.files(output_dir), output_dir)
+  create_arrow_files_and_tss("hg19", 
+                             setdiff(list.files(output_dir, full.names=T),
+                                     list.dirs(output_dir, recursive = F, 
+                                               full.names = T)), 
+                             output_dir)
 }
