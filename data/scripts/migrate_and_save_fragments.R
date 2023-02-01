89ebdc8ba6e0f5211f58508922b1ee3464bae089
Mohamad Bairakdar <mdanb@umich.edu>
2023-02-01 18:49:23 -0500

    migrate script

---
+++ data/scripts/migrate_and_save_fragments.R
@@ -0,0 +1,59 @@
+library(parallel)
+library(rtracklayer)
+source("create_count_overlaps_utils.R")
+
+option_list <- list(
+  make_option("--dataset", type="character"),
+  make_option("--cores", type="integer")
+)
+
+args = parse_args(OptionParser(option_list=option_list))
+
+dataset = args$dataset
+cores = args$cores
+ch = import.chain("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/hg38ToHg19.over.chain")
+
+if (dataset == "bing_ren") {
+  dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/bingren_scATAC/migrated_to_hg19"
+  dir.create(dir_path)
+  files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/",
+                     pattern="tsv.gz",
+                     full.names=TRUE)
+  fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+                                mc.cores=cores)
+  
+} else if (dataset == "tsankov") {
+  dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/Tsankov_scATAC/migrated_to_hg19"
+  dir.create(dir_path)
+  files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/Tsankov_scATAC",
+                     pattern="tsv.gz",
+                     full.names=TRUE)
+  fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+                                mc.cores=cores)
+} else if (dataset == "greenleaf_brain") {
+  dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_brain_scATAC/migrated_to_hg19"
+  dir.create(dir_path)
+  files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_brain_scATAC",
+                     pattern="tsv.gz",
+                     full.names=TRUE)
+  fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+                                mc.cores=cores)
+} else if (dataset == "greenleaf_pbmc_bm") {
+  dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_pbmc_bm_scATAC/migrated_to_hg19"
+  dir.create(dir_path)
+  files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_pbmc_bm_scATAC",
+                     pattern="tsv.gz",
+                     full.names=TRUE)
+  fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+                                mc.cores=cores)
+}
+
+migrated_filepaths = paste(dir_path, gsub(".gz", "", migrated_filenames),
+                           sep = "/")
+                           
+mclapply(fragments, export, format="bed", index=T)
+
