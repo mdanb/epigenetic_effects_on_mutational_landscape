bd1690315e33494270cf1384c4cf1b90c06f55aa
bgiotti <brunogiotti@gmail.com>
2023-02-02 00:55:52 +0000

    archr qc

--- analysis/ArchR_analysis/create_archr_qc_plots.R
+++ analysis/ArchR_analysis/create_archr_qc_plots.R
@@ -3,9 +3,41 @@ library(ArchR)
 addArchRThreads(threads = 8)
 addArchRGenome("hg19")
 
-ArrowFiles = list.files(
-ArchR_proj <- ArchRProject(ArrowFiles = ArrowFiles, 
-    			   outputDirectory = "ArchR_analysis",
-     			   copyArrows = TRUE)
+ArrowFiles = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/arrow", 
+			recursive=T,
+			full.names=T,
+			pattern=".arrow")
+ArchR_proj <- ArchRProject(ArrowFiles = ArrowFiles,
+			   copyArrows=F,
+    			   outputDirectory = "/ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/analysis/ArchR_analysis/ArchR_project_dir")
 
+dataset = unlist(lapply(strsplit(ArrowFiles, split = "/"), "[", 8))
+ArchR_proj = addSampleColData(ArchRProj = ArchR_proj, 
+			      data=dataset, name="dataset", 
+		    	      samples=rownames(getSampleColData(ArchRProj = ArchR_proj)))
+
+sample_col_data = getSampleColData(ArchRProj = ArchR_proj)
+#bingren = sample_col_data[sample_col_data$dataset == "bingren", ]
+#samples = rownames(bingren)
+samples = rownames(sample_col_data)
+samples_per_cell = unlist(lapply(strsplit(rownames(getCellColData(ArchRProj = ArchR_proj)), 
+			  split = "#"), "[", 1))
+idx = match(samples_per_cell, samples)
+dataset_per_cell = dataset[idx]
+ArchR_proj = addCellColData(ArchRProj = ArchR_proj,
+                            data=dataset_per_cell, name="dataset",
+			    cells=rownames(getCellColData(ArchRProj = ArchR_proj)))							    
+#bingren_cells = ArchR_proj
+
+p2 <- plotGroups(
+    ArchRProj = ArchR_proj, 
+    groupBy = "Sample", 
+    colorBy = cellColData,
+    name = "TSSEnrichment",
+    plotAs = "violin",
+    alpha = 0.4,
+    addBoxPlot = TRUE,
+    ratioYX=1/3)
+
+plotPDF(p2, name = "QC-Sample-Statistics.pdf", ArchRProj = ArchR_proj, addDOC = FALSE, width = 20, height = 10)
 
