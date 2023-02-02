07793c1344ba43d194d769c93664d3d3d06d6683
bgiotti <brunogiotti@gmail.com>
2023-02-02 05:59:34 +0000

    f

--- data/scripts/create_arrow_files_and_tss.R
+++ data/scripts/create_arrow_files_and_tss.R
@@ -45,7 +45,7 @@ create_arrow_files_and_tss <- function(genome_version, fragment_paths,
 if (dataset == "bing_ren") {
   output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/arrow/bingren" 
   files_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/bingren_scATAC/migrated_to_hg19"
-  create_arrow_files_and_tss("hg38", list.files(output_dir, full.names=T,
+  create_arrow_files_and_tss("hg19", list.files(output_dir, full.names=T,
                                                 pattern = "bgz$"),
 						output_dir)
 						
@@ -57,18 +57,18 @@ if (dataset == "bing_ren") {
 } else if (dataset == "greenleaf_pbmc_bm") {
    output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/arrow/greenleaf_pbmc_bm"
    files_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_pbmc_bm_scATAC/migrated_to_hg19"
-   create_arrow_files_and_tss("hg38", list.files(files_dir, full.names=T,
+   create_arrow_files_and_tss("hg19", list.files(files_dir, full.names=T,
                                pattern = "bgz$"), output_dir)
 } else if (dataset == "greenleaf_brain") {
    output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/arrow/greenleaf_brain"
    files_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_brain_scATAC/migrated_to_hg19"
-   create_arrow_files_and_tss("hg38", list.files(files_dir, full.names=T,
+   create_arrow_files_and_tss("hg19", list.files(files_dir, full.names=T,
 	                          pattern = "bgz$"), output_dir)
 
 } else if (dataset == "tsankov") {
    output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/arrow/tsankov"
    files_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/Tsankov_scATAC/migrated_to_hg19"
-   create_arrow_files_and_tss("hg38", list.files(files_dir, full.names=T,
+   create_arrow_files_and_tss("hg19", list.files(files_dir, full.names=T,
 	                                   pattern = "bgz$"), output_dir)
 } else if (dataset == "yang_kidney") {
     output_dir = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/arrow/yang_kidney"
