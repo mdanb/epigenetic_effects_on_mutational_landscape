69d85f6e89d0d5deb0bcb20506189e9e9482702e
Mohamad Bairakdar <mdanb@umich.edu>
2023-02-02 01:11:16 -0500

    migrate and save

--- data/scripts/migrate_and_save_fragments.R
+++ data/scripts/migrate_and_save_fragments.R
@@ -14,15 +14,29 @@ dataset = args$dataset
 cores = args$cores
 ch = import.chain("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/hg38ToHg19.over.chain")
 
+helper <- function(files, dir_path, ch, cores) {
+  fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+                                mc.cores=cores)
+  files = lapply(strsplit(files, split = "/"), "[", 9)
+  migrated_filepaths = paste(dir_path, gsub(".gz", "", files),
+                             sep = "/")
+  
+  mclapply(seq_along(migrated_fragments), function(i) {export(migrated_fragments[[i]],
+                                                           migrated_filepaths[i],
+                                                           format="bed",
+                                                           index=T)})
+}
+
 if (dataset == "bing_ren") {
   dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/bingren_scATAC/migrated_to_hg19"
   dir.create(dir_path)
   files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/bingren_scATAC",
                      pattern="bed.gz",
                      full.names=TRUE)
-  fragments = mclapply(files, import, format="bed", mc.cores=cores)
-  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
-                                mc.cores=cores)
+  for (l in split(files, ceiling(seq_along(files)/8))) {
+    helper(l, dir_path, ch, cores)
+  }
   
 } else if (dataset == "tsankov") {
   dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/Tsankov_scATAC/migrated_to_hg19"
@@ -30,35 +44,35 @@ if (dataset == "bing_ren") {
   files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/Tsankov_scATAC",
                      pattern="tsv.gz",
                      full.names=TRUE)
-  fragments = mclapply(files, import, format="bed", mc.cores=cores)
-  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
-                                mc.cores=cores)
+  for (l in split(files, ceiling(seq_along(files)/8))) {
+    helper(l, dir_path, ch, cores)
+  }
+  # fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  # migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+  #                               mc.cores=cores)
 } else if (dataset == "greenleaf_brain") {
   dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_brain_scATAC/migrated_to_hg19"
   dir.create(dir_path)
   files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_brain_scATAC",
                      pattern="tsv.gz",
                      full.names=TRUE)
-  fragments = mclapply(files, import, format="bed", mc.cores=cores)
-  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
-                                mc.cores=cores)
+  for (l in split(files, ceiling(seq_along(files)/8))) {
+    helper(l, dir_path, ch, cores)
+  }
+  # fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  # migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+  #                               mc.cores=cores)
 } else if (dataset == "greenleaf_pbmc_bm") {
   dir_path = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_pbmc_bm_scATAC/migrated_to_hg19"
   dir.create(dir_path)
   files = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/raw_dir/bed_files/greenleaf_pbmc_bm_scATAC",
                      pattern="tsv.gz",
                      full.names=TRUE)
-  fragments = mclapply(files, import, format="bed", mc.cores=cores)
-  migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
-                                mc.cores=cores)
+  for (l in split(files, ceiling(seq_along(files)/8))) {
+    helper(l, dir_path, ch, cores)
+  }
+  # fragments = mclapply(files, import, format="bed", mc.cores=cores)
+  # migrated_fragments = mclapply(fragments, migrate_bed_file_to_hg37, ch, 
+  #                               mc.cores=cores)
 }
 
-files = lapply(strsplit(files, split = "/"), "[", 9)
-migrated_filepaths = paste(dir_path, gsub(".gz", "", files),
-                           sep = "/")
-                           
-mclapply(length(migrated_fragments), function(i) {export(migrated_fragments[i],
-                                                         migrated_filepaths[i],
-                                                         format="bed",
-                                                         index=T)})
-         
