52fc6c374ca78187f5d1115b3a7c8905e3535822
bgiotti <brunogiotti@gmail.com>
2023-02-03 17:33:20 +0000

    f

--- data/scripts/tsse_filtered_run_commands/for_ML_input/RPL_tsankov.sh
+++ data/scripts/tsse_filtered_run_commands/for_ML_input/RPL_tsankov.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #$ -cwd
-#$ -l h_rt=24:00:00
+#$ -l h_rt=48:00:00
 #$ -l os=RedHat7
 #$ -pe smp 1
 #$ -l h_vmem=128G
 #$ -N RPL_tsankov
-#$ -o /ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/tsse_filtered_run_commands/for_ML_input/log_dir/RPL_tsankov.out
-#$ -e /ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/tsse_filtered_run_commands/for_ML_input/log_dir/RPL_tsankov.err
+#$ -o /ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/data/scripts/tsse_filtered_run_commands/for_ML_input/log_dir/RPL_tsankov.out
+#$ -e /ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/data/scripts/tsse_filtered_run_commands/for_ML_input/log_dir/RPL_tsankov.err
 #$ -binding linear:1
 #$ -M bgiotti@broadinstitute.org
 #$ -m bea
@@ -15,4 +15,4 @@ source /broad/software/scripts/useuse
 use .anaconda3-5.3.1
 source activate /home/unix/bgiotti/conda/coo
 
-Rscript /ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/create_tsse_filtered_count_overlaps.R --cell_types='SmoothMuscle;Fibroblasts;AT2;Immune;AT1;Endothelial;Ciliated;Mesothelium;Secretory;B_cells' --dataset='tsankov' --top_tsse_fragment_count_range='100000,150000,200000' --files_pattern='RPL' --cores=1
+Rscript /ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/data/scripts/create_tsse_filtered_count_overlaps.R --cell_types='SmoothMuscle;Fibroblasts;AT2;Immune;AT1;Endothelial;Ciliated;Mesothelium;Secretory;B_cells' --dataset='tsankov' --top_tsse_fragment_count_range='100000,150000,200000,300000,400000,500000,1000000' --files_pattern='RPL' --cores=1
