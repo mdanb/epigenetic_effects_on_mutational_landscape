a7d149c53fd327aab6ddf1f1d964a964be12c0ca
Mohamad Bairakdar <mdanb@umich.edu>
2023-02-06 18:43:59 -0500

    f

--- data/scripts/create_tsse_filtered_count_overlaps.R
+++ data/scripts/create_tsse_filtered_count_overlaps.R
@@ -436,7 +436,7 @@ if (dataset == "Bingren") {
                                     pattern="RPL.*[.]gz")
   if (files_pattern == "RPL") {
     print("Tsankov RPL")
-    files_Tsankov_distal = c("RPL_280_neg_1_fragments.tsv.gz", "RPL_280_neg_2_fragments.tsv.gz")
+    # files_Tsankov_distal = c("RPL_280_neg_1_fragments.tsv.gz", "RPL_280_neg_2_fragments.tsv.gz")
     create_tsse_filtered_count_overlaps_per_tissue(files=files_Tsankov_distal,
                                                    metadata=metadata_tsankov_distal,
                                                    interval_ranges=interval.ranges,
