fe5852624cc6ac7fe05a02d1e850e79b96d193eb
Mohamad Bairakdar <mdanb@umich.edu>
2023-02-09 17:32:55 -0500

    f

--- analysis/ArchR_analysis/create_archr_qc_plots.R
+++ analysis/ArchR_analysis/create_archr_qc_plots.R
@@ -1,5 +1,6 @@
 library(ArchR)
 library(optparse)
+library(tibble)
 
 option_list <- list(
   make_option("--cores", type="integer")
@@ -17,7 +18,7 @@ ArrowFiles = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/arrow",
                     			pattern=".arrow")
 ArchR_proj <- ArchRProject(ArrowFiles = ArrowFiles,
 			                     copyArrows=F,
-    			      outputDirectory = "/ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/analysis/ArchR_analysis/ArchR_project_dir")
+    			      outputDirectory = "/broad/hptmp/bgiotti/BingRen_scATAC_atlas/analysis/ArchR_QC")
 
 dataset = unlist(lapply(strsplit(ArrowFiles, split = "/"), "[", 8))
 ArchR_proj = addSampleColData(ArchRProj = ArchR_proj, 
@@ -34,24 +35,28 @@ samples_per_cell = unlist(lapply(strsplit(rownames(getCellColData(ArchRProj = Ar
 idx = match(samples_per_cell, samples)
 dataset_per_cell = dataset[idx]
 ArchR_proj = addCellColData(ArchRProj = ArchR_proj,
-                            data=dataset_per_cell, name="dataset",
+                            data=dataset_per_cell, name="dataset_per_cell",
 			                      cells=rownames(getCellColData(ArchRProj = ArchR_proj)))
 
-ggplot(ArchR_proj$cellColData, aes(x = Sample, y = TSSEnrichment)) +
+ggplot(as_tibble(getCellColData(ArchR_proj)), 
+        aes(x = Sample, y = TSSEnrichment)) +
         geom_boxplot(aes(fill = dataset_per_cell), outlier.size=.2) +
         theme (axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
         xlab('Sample') + 
-        ylab('TSSEnrichment')  
+        ylab('TSSEnrichment') +
+        labs(fill="dataset")
 
-ggsave("../../figures/archr_tss_vs_sample.png")
+ggsave("../../figures/archr_tss_vs_sample.png", width=20)
 
-ggplot(ArchR_proj$cellColData, aes(x = Sample, y = TSSEnrichment)) +
-  geom_boxplot(aes(fill = dataset), outlier.size=.2) +
-  theme (axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  xlab('Sample') + 
-  ylab('Log(num fragments)')
+ggplot(as_tibble(getCellColData(ArchR_proj)), 
+       aes(x = Sample, y = log10(nFrags))) +
+       geom_boxplot(aes(fill = dataset_per_cell), outlier.size=.2) +
+       theme (axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+       xlab('Sample') + 
+       ylab('Log(num fragments)') +
+       labs(fill="dataset")
 
-ggsave("../../figures/archr_log_frags_vs_sample.png")
+ggsave("../../figures/archr_log_frags_vs_sample.png", width=20)
 #bingren_cells = ArchR_proj
 
 # tss_p <-plotGroups(
