6c5879ed16fe39f938fb7c729550a9c6137ae379
Mohamad Bairakdar <mdanb@umich.edu>
2023-04-20 19:56:19 -0400

    f

--- data/scripts/create_count_overlaps.R
+++ data/scripts/create_count_overlaps.R
@@ -168,18 +168,23 @@ if (dataset == "Bingren") {
     metadata_bingren = read.table("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/metadata/GSE184462_metadata.tsv", 
                           sep="\t",
                           header=T)
-    colnames(metadata_bingren)[grepl("cell.type", colnames(metadata_bingren))] = "cell_type"
-    files_bingren = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/bed_files/bingren_scATAC",
-                                pattern=".*fragments\\.bed\\.bgz")
-    
-    mclapply(files_bingren, create_count_overlaps_files,
-             cell_number_filter=cell_number_filter,
-             metadata=metadata_bingren,
-             interval_ranges=interval.ranges,
-             chain=ch,
-             dataset=dataset,
-             annotation=annotation,
-             mc.cores=cores)
+  else if (annotation == "bingren_remove_same_celltype_indexing") {
+    metadata_bingren = read.table("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/metadata/bingren_remove_same_celltype_indexing.csv", 
+                                  sep=",",
+                                  header=T)
+  }
+  colnames(metadata_bingren)[grepl("cell.type", colnames(metadata_bingren))] = "cell_type"
+  files_bingren = list.files("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/bed_files/bingren_scATAC/migrated_to_hg19",
+                              pattern=".*fragments\\.bed\\.bgz")
+  
+  mclapply(files_bingren, create_count_overlaps_files,
+           cell_number_filter=cell_number_filter,
+           metadata=metadata_bingren,
+           interval_ranges=interval.ranges,
+           chain=ch,
+           dataset=dataset,
+           annotation=annotation,
+           mc.cores=cores)
   }
 } else if (dataset == "Shendure") {
   metadata_Shendure = read.table("/broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/metadata/GSE149683_File_S2.Metadata_of_high_quality_cells.txt",
