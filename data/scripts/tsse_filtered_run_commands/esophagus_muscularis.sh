c895f24b95d0e301c465cb49a98d3eb2732f4ba2
bgiotti <brunogiotti@gmail.com>
2023-05-16 16:24:28 -0400

    f

--- data/scripts/tsse_filtered_run_commands/esophagus_muscularis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript ../create_tsse_filtered_count_overlaps.R --top_tsse_fragment_count_range="1000,10000,50000,100000,150000,250000,300000,322928,322929,400000,500000,600000" --dataset="bing_ren" --cell_types="Foveolar Cell" --files_pattern="esophagus_muscularis" --cores=1
