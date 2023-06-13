fcccd16123b74cdf64be389f8b172cf411ff44bf
mdanb <mdanb@umich.edu>
2023-06-13 19:30:26 -0400

    change color

--- analysis/ArchR_analysis/reannotate_datasets.R
+++ analysis/ArchR_analysis/reannotate_datasets.R
@@ -534,7 +534,7 @@ if (plot_cell_types) {
   }
   cols <- c("#e6194B", "#3cb44b", "#ffe119", "#4363d8", "#f58231",
             "#911eb4", "#42d4f4", "#f032e6", "#bfef45", "#fabed4",
-            "#469990", "#dcbeff", "#9A6324", "#fffac8", "#800000",
+            "#469990", "#dcbeff", "#9A6324", "#7F00FF", "#800000",
             "#aaffc3", "#808000", "#ffd8b1", "#000075", "#000000")
   p <- p + 
     scale_color_manual(values = cols,
