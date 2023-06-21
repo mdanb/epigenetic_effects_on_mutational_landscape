fd1fd556b6fc4e3f1c04ddfd0c5b3551d0d14018
mdanb <mdanb@umich.edu>
2023-06-21 09:26:49 -0400

    f

--- analysis/ML/ML_utils.py
+++ analysis/ML/ML_utils.py
@@ -250,13 +250,12 @@ def backward_eliminate_features(X_train, y_train, backwards_elim_dir,
 #### Model train/val/test helpers ####
 def optimize_optuna_study(study_name, ML_model, X_train, y_train, seed, n_optuna_trials):
     storage_name = "mysql+pymysql://mdanb:mdanb@localhost:3306/optuna_db"
-    subprocess.call(["mysqld_safe",
+    subprocess.call(["nohup mysqld_safe",
                      f"--socket=/broad/hptmp/bgiotti/BingRen_scATAC_atlas/analysis/ML/mysql.sock",
                      f"--log-error=/broad/hptmp/bgiotti/BingRen_scATAC_atlas/analysis/ML/mysql.log",
                      f"--datadir=/broad/hptmp/bgiotti/BingRen_scATAC_atlas/analysis/ML/mysql_data",
                      f"--pid-file=/broad/hptmp/bgiotti/BingRen_scATAC_atlas/analysis/ML/mariadb.pid"
                      ])
-    subprocess.call(["wait"])
     # storage_name = "sqlite:///example.db"
 
     study = optuna.create_study(direction="maximize",
