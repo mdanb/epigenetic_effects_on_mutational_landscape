a04e1b8173003616e10378ff500c7087deeab682
mdanb <mdanb@umich.edu>
2023-07-13 16:14:20 -0400

    f

--- analysis/ML/ML_utils.py
+++ analysis/ML/ML_utils.py
@@ -332,9 +332,6 @@ def backward_eliminate_features(X_train, y_train, backwards_elim_dir,
                                                                                    y_train, seed, df,
                                                                                    best_cv_score=best_cv_score,
                                                                                    fp_for_fi=fp_for_fi)
-        print(top_n_feats)
-        print(feature_importances)
-
         X_train = X_train.loc[:, top_n_feats]
         if not os.path.exists(filepath):
             print_and_save_features(top_n_feats, filepath=filepath, top=True)
@@ -577,7 +574,6 @@ def call_plot_top_features(seed, cancer_types_arg, ML_model, datasets_arg, scATA
     #     command = command.append("--RNA_subtyped")
     # elif per_donor:
     #     command = command.append("--per_donor")
-    print(command)
     subprocess.call(command)
     print(f"Done plotting top features for seed {seed}!")
 
