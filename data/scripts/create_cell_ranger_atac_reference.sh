06b9c47d885bef2c08b08045d58d067ed2ae9c59
mdanb <mdanb@umich.edu>
2023-08-09 12:14:12 -0400

    d

--- data/scripts/create_cell_ranger_atac_reference.sh
+++ data/scripts/create_cell_ranger_atac_reference.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-#/seq/regev_genome_portal/SOFTWARE/10X/cellranger-3.1.0/cellranger mkgtf /broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/Homo_sapiens.GRCh37.87.gtf /broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/Homo_sapiens.GRCh37.87.filtered.gtf \
-#                 --attribute=gene_biotype:protein_coding \
-#                 --attribute=gene_biotype:lincRNA \
-#                 --attribute=gene_biotype:antisense
-
-cd /broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/
-/ahg/regevdata/projects/ICA_Lung/Mohamad/cell_of_origin/software/cellranger-atac-2.1.0/cellranger-atac mkref --config=GRCh37.config 
+cd .. 
+cellranger mkgtf Homo_sapiens.GRCh37.87.gtf Homo_sapiens.GRCh37.87.filtered.gtf \
+                 --attribute=gene_biotype:protein_coding \
+                 --attribute=gene_biotype:lincRNA \
+                 --attribute=gene_biotype:antisense
+cellranger mkref --genome=hg19 \
+                 --fasta=Homo_sapiens.GRCh37.dna.primary_assembly.fa \
+                 --genes=Homo_sapiens.GRCh37.87.filtered.gtf \
+                 --ref-version=3.0.0
