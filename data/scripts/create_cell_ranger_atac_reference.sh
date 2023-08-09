42915b92ad7bfe304134c3809825f174d5a043ef
bgiotti <brunogiotti@gmail.com>
2023-08-09 16:29:37 +0000

    f

--- data/scripts/create_cell_ranger_atac_reference.sh
+++ data/scripts/create_cell_ranger_atac_reference.sh
@@ -1,9 +1,9 @@
 cd .. 
-cellranger mkgtf Homo_sapiens.GRCh37.87.gtf Homo_sapiens.GRCh37.87.filtered.gtf \
+/seq/regev_genome_portal/SOFTWARE/10X/cellranger-3.1.0/cellranger mkgtf Homo_sapiens.GRCh37.87.gtf Homo_sapiens.GRCh37.87.filtered.gtf \
                  --attribute=gene_biotype:protein_coding \
                  --attribute=gene_biotype:lincRNA \
-                 --attribute=gene_biotype:antisense
-cellranger mkref --genome=hg19 \
+                 --attribute=gene_biotype:antisense mkref --genome=hg19 \
+/seq/regev_genome_portal/SOFTWARE/10X/cellranger-3.1.0/cellranger mkref --genome=hg19 \
                  --fasta=Homo_sapiens.GRCh37.dna.primary_assembly.fa \
                  --genes=Homo_sapiens.GRCh37.87.filtered.gtf \
-                 --ref-version=3.0.0
+                 --ref-version=3.1.0
