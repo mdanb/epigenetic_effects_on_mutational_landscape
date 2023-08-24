a39ea2f5ba5ca15d979d31d2132ab6d6255cbe22
mdanb <mdanb@umich.edu>
2023-08-24 16:24:22 -0400

    ad

--- post_installation.R
+++ post_installation.R
@@ -1,8 +1,12 @@
+
 # Install devtools if not already installed
 if (!requireNamespace("devtools", quietly = TRUE)) {
-  install.packages("devtools")
+  install.packages("devtools", version="2.3.2")
 }
 
+library(devtools)
+
 # Install ArchR from GitHub
-devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
+devtools::install_github("GreenleafLab/ArchR", ref="v1.0.1", 
+                         repos = BiocManager::repositories())
 
