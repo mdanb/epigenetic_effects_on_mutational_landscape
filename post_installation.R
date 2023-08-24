ab441ddda1c53f691e0a0c4fea8ac21b2ce65d3f
mdanb <mdanb@umich.edu>
2023-08-24 19:01:22 -0400

    add Cran

--- post_installation.R
+++ post_installation.R
@@ -1,13 +1,15 @@
+# Set default CRAN mirror
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org/"
+  options(repos = r)
+})
 
-# Install devtools if not already installed
 if (!requireNamespace("devtools", quietly = TRUE)) {
   install.packages("devtools", version="2.3.2")
 }
 
 library(devtools)
-
-
-# Install ArchR from GitHub
 devtools::install_github("GreenleafLab/ArchR", ref="v1.0.1", 
                          repos = BiocManager::repositories(), dependencies=T)
 remotes::install_github("satijalab/seurat", "seurat5", quiet = TRUE, dependencies=T)
