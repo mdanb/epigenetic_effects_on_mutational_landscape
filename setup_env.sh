77a22370ea30a7f1afc92533b02b616c598703c0
mdanb <mdanb@umich.edu>
2023-08-24 22:56:47 -0400

    f

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
-if mamba info --envs | grep -q coo; then
-	mamba env update -f environment.yml
-else 
-	mamba env create -f environment.yml
-fi
+#if mamba info --envs | grep -q coo; then
+#	mamba env update -f environment.yml
+#else 
+#	mamba env create -f environment.yml
+#fi
 mamba activate coo
 Rscript post_installation.R
