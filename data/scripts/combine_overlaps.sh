ebc87bccb1718d6bf1bd5a755f56df345f05b3d1
bgiotti <brunogiotti@gmail.com>
2023-09-12 14:54:25 +0000

    backup

--- data/scripts/combine_overlaps.sh
+++ data/scripts/combine_overlaps.sh
@@ -1,1 +1,16 @@
-Rscript combine_overlaps.R --datasets=Tsankov --annotation=Tsankov_refined --which_interval_ranges=polak
+#!/bin/bash
+#$ -cwd
+#$ -l h_rt=336:00:00
+#$ -l os=RedHat7
+#$ -pe smp 1
+#$ -l h_vmem=64G
+#$ -N combine_ovs_per_cell
+#$ -o /broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/scripts
+#$ -e /broad/hptmp/bgiotti/BingRen_scATAC_atlas/data/scripts
+#$ -binding linear:8
+
+source /broad/software/scripts/useuse
+use Anaconda
+source activate /home/unix/bgiotti/conda/coo
+
+Rscript combine_overlaps.R --datasets=Greenleaf_colon --annotation=default_annotation --which_interval_ranges=polak --overlaps_per_cell
