28ab0e3b90700b6228e7e27e50616813d73c1d75
mdanb <mdanb@umich.edu>
2023-09-26 18:30:41 -0400

    d

--- analysis/ML/post_modeling_analysis.py
+++ analysis/ML/post_modeling_analysis.py
@@ -1,11 +1,16 @@
 from ML_utils import *
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from config import create_parser
+from scipy.stats import zscore, norm
+from statsmodels.stats.multitest import multipletests
+import os.path as osp
+# import pyreadr
 
 parser = create_parser()
-parser.add_argument('--error_analysis', action="store_true", default=False)
+parser.add_argument('--bins_error_analysis', action="store_true", default=False)
 parser.add_argument('--count_bin_sums', action="store_true", default=False)
+parser.add_argument('--error_analysis_num_features', type=int, default=1)
 
 config = parser.parse_args()
 
@@ -23,66 +28,106 @@ histologically_subtyped_mutations = config.histologically_subtyped_mutations
 de_novo_seurat_clustering = config.de_novo_seurat_clustering
 meso = config.meso
 RNA_subtyped = config.RNA_subtyped
+hundred_kb = config.hundred_kb
 per_donor = config.per_donor
 donor_range = config.donor_range
-seed_range = config.seed_range
-start, end = map(int, seed_range.split('-'))
-seed_range = range(start, end + 1)
+# seed_range = config.seed_range
+# start, end = map(int, seed_range.split('-'))
+# seed_range = range(start, end + 1)
 n_optuna_trials_prebackward_selection = config.n_optuna_trials_prebackward_selection
 n_optuna_trials_backward_selection = config.n_optuna_trials_backward_selection
 feature_importance_method = config.feature_importance_method
 tss_filter = config.tss_fragment_filter
-num_features = config.error_analysis_num_features
+error_analysis_num_features = config.error_analysis_num_features
 fold_for_test_set = config.fold_for_test_set
-error_analysis = config.error_analysis
+bins_error_analysis = config.bins_error_analysis
 count_bin_sums = config.count_bin_sums
 
-if error_analysis:
-    for seed in seed_range:
-        for cancer_type in cancer_types:
-            scATAC_df, cancer_specific_mutations = load_data(meso, SCLC, lung_subtyped, woo_pcawg,
-                                                              histologically_subtyped_mutations,
-                                                              de_novo_seurat_clustering,
-                                                              CPTAC, combined_CPTAC_ICGC, RNA_subtyped, per_donor,
-                                                              datasets, scATAC_cell_number_filter,
-                                                              annotation_dir, cancer_type)
-            scATAC_sources = construct_scATAC_sources(datasets)
-            scATAC_dir = construct_scATAC_dir(scATAC_sources, scATAC_cell_number_filter, tss_filter, annotation_dir, seed,
-                                              fold_for_test_set)
-
-            model = load_n_features_backwards_elim_models(num_features, scATAC_df.shape[1], cancer_type, ML_model,
-                                                          scATAC_dir, feature_importance_method, full_data_trained=True)
-            top_features = model.feature_names_in_
-            scATAC_df = scATAC_df.loc[:, top_features]
-            # X_train, _, y_train, _ = get_train_test_split(scATAC_df, cancer_specific_mutations, 0.10, seed)
-            errors = apply_func_to_kfolds(scATAC_df, cancer_specific_mutations, compute_error, estimator=model,
-                                          train_new_model=True)
+if bins_error_analysis:
+#     for seed in seed_range:
+    for cancer_type in cancer_types:
+        scATAC_df, cancer_specific_mutations = load_data(meso, SCLC, lung_subtyped, woo_pcawg,
+                                                          histologically_subtyped_mutations,
+                                                          de_novo_seurat_clustering,
+                                                          CPTAC, combined_CPTAC_ICGC, RNA_subtyped, per_donor,
+                                                          datasets, scATAC_cell_number_filter,
+                                                          annotation_dir, cancer_type, hundred_kb)
+        scATAC_sources = construct_scATAC_sources(datasets)
+        multiseed_errors = []
+        multiseed_percent_errors = []
+        multiseed_preds = []
+        for seed in range(1, 11):
             abs_errors = []
             percent_errors = []
             preds = []
-            for fold_results in errors:
-                abs_errors.append(fold_results["abs_err"])
-                percent_errors.append(fold_results["percent_err"])
-                preds.append(fold_results["preds"])
-            abs_errors = pd.concat(abs_errors)
-            percent_errors = pd.concat(percent_errors)
-            preds = np.concatenate(preds)
-            # errors = [err for err_list in errors for err in err_list]
-            fig, ax = plt.subplots(5, 1, figsize=(15,15), sharex=True)
-            x = np.arange(len(abs_errors))
-            to_plot = [abs_errors, percent_errors, cancer_specific_mutations, preds, scATAC_df.to_numpy().reshape(-1)]
-            ylabel_list = ["Absolute error", "Percent error", "Mutation counts", "Predictions", "scATAC"]
-            for i in range(5):
-                ax[i].bar(x, np.asarray(to_plot[i], dtype=np.float32), width=4)
-                # for j in range(0, 2200, 213):
-                #     ax[i].axvline(x=j, c="red")
-                ax[i].set_ylabel(ylabel_list[i])
-            # plt.title("Percent error versus chromosomal bins")
-            ax[-1].set_xticks(np.arange(min(x), max(x)+1, 100))
-            ax[-1].set_xlabel("Bin")
-            for a in ax:
-                a.tick_params(axis='x', labelsize=8)
-            plt.savefig(f"../../figures/models/{ML_model}/{cancer_type}/{scATAC_dir}/errors.png")
+            for fold_for_test_set in range(0, 10):
+                scATAC_dir = construct_scATAC_dir(scATAC_sources, scATAC_cell_number_filter, tss_filter, annotation_dir,
+                                                  seed, fold_for_test_set)
+
+                model = load_n_features_backwards_elim_models(error_analysis_num_features, scATAC_df.shape[1],
+                                                              cancer_type, ML_model, scATAC_dir, feature_importance_method,
+                                                              full_data_trained=True)
+                top_features = model.feature_names_in_
+                _, X_test, _, y_test = get_train_test_split(scATAC_df, cancer_specific_mutations, 10, fold_for_test_set)
+                errors = compute_error(X_test.loc[:, top_features], y_test, estimator=model, train_new_model=False,
+                                       i=fold_for_test_set)
+                # errors = apply_func_to_kfolds(scATAC_df.loc[:, top_features], cancer_specific_mutations, compute_error,
+                #                               estimator=model,
+                #                               train_new_model=False)
+                # for fold_results in errors:
+                abs_errors.append(errors["abs_err"])
+                percent_errors.append(errors["percent_err"])
+                preds.append(errors["preds"])
+            multiseed_errors.append(pd.concat(abs_errors))
+            multiseed_percent_errors.append(pd.concat(percent_errors))
+            multiseed_preds.append(np.concatenate(preds))
+
+        multiseed_errors = pd.concat(multiseed_errors, axis=1).mean(axis=1)
+        multiseed_percent_errors = pd.concat(multiseed_percent_errors, axis=1).mean(axis=1)
+        # multiseed_percent_errors.plot.hist()
+        normalized_percent_errors = zscore(multiseed_percent_errors)
+        p_values = 2 * (1 - norm.cdf(abs(normalized_percent_errors)))
+        rejected, q_values, _, _ = multipletests(p_values, alpha=0.05, method='fdr_bh')
+        df = pd.DataFrame({"p-value": p_values,
+                           "absolute_error": multiseed_errors, "percent_error":multiseed_percent_errors,
+                           "normalized_percent_error": normalized_percent_errors, "q-value":q_values,
+                           "rejected":rejected})
+        fp = construct_scATAC_dir(scATAC_sources, scATAC_cell_number_filter, tss_filter, annotation_dir,
+                                  all_seeds=True)
+        # Good style / Cool pattern
+        current_fp = osp.dirname(osp.realpath(__file__))
+        fp = osp.join(current_fp, "..", "..", "figures", "models", ML_model,
+                      cancer_type, fp, "errors_df.csv")
+        df = df.sort_values(by="q-value")
+        # if hundred_kb:
+        #     ranges = pyreadr.read_r(osp.join(current_fp, "..", "..", "data", "100kb_interval_ranges.Rdata"))
+        # else:
+        #     ranges = pyreadr.read_r(osp.join(current_fp, "..", "..", "data", "mutation_data",
+        #                                      "hg19.1Mb.ranges.Polak.Nature2015.RData"))
+        df.to_csv(fp)
+
+        # multiseed_preds = np.average(multiseed_preds, axis=0)
+
+
+            # abs_errors = pd.concat(abs_errors)
+            # percent_errors = pd.concat(percent_errors)
+            # preds = np.concatenate(preds)
+        # # errors = [err for err_list in errors for err in err_list]
+        # fig, ax = plt.subplots(5, 1, figsize=(15,15), sharex=True)
+        # x = np.arange(len(abs_errors))
+        # to_plot = [abs_errors, percent_errors, cancer_specific_mutations, preds, scATAC_df.to_numpy().reshape(-1)]
+        # ylabel_list = ["Absolute error", "Percent error", "Mutation counts", "Predictions", "scATAC"]
+        # for i in range(5):
+        #     ax[i].bar(x, np.asarray(to_plot[i], dtype=np.float32), width=4)
+        #     # for j in range(0, 2200, 213):
+        #     #     ax[i].axvline(x=j, c="red")
+        #     ax[i].set_ylabel(ylabel_list[i])
+        # # plt.title("Percent error versus chromosomal bins")
+        # ax[-1].set_xticks(np.arange(min(x), max(x)+1, 100))
+        # ax[-1].set_xlabel("Bin")
+        # for a in ax:
+        #     a.tick_params(axis='x', labelsize=8)
+        # plt.savefig(f"../../figures/models/{ML_model}/{cancer_type}/{scATAC_dir}/errors.png")
 
 # Use only seed=1 since splits are the same for all seeds
 if count_bin_sums:
