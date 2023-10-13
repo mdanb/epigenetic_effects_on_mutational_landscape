57494e3a16b5e6d485f570efb7b5450a47a5b255
mdanb <mdanb@umich.edu>
2023-10-13 16:45:27 -0400

    f

--- data/scripts/create_count_overlaps.R
+++ data/scripts/create_count_overlaps.R
@@ -387,6 +387,7 @@ if (dataset == "Bingren") {
            dataset=dataset,
            annotation=annotation,
            which_interval_ranges=which_interval_ranges,
+           overlaps_per_cell=overlaps_per_cell,
            mc.cores=cores)
 } else if (dataset == "Greenleaf_pbmc_bm") {
     if (!(file.exists("../metadata/greenleaf_pbmc_bm.txt"))) {
