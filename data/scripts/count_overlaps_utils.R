cb34573e7c22b37c41033dd3cabbd3eac14395c0
mdanb <mdanb@umich.edu>
2023-10-25 22:06:48 -0400

    sep _

--- data/scripts/count_overlaps_utils.R
+++ data/scripts/count_overlaps_utils.R
@@ -307,7 +307,7 @@ get_tissue_name_greenleaf_colon <- function(file, metadata) {
     sample = paste(sample, collapse="-")
   }
   type = metadata[metadata["Sample"] == sample, "GrossPathology"] %>% pull()
-  return(paste(type, "colon"))
+  return(paste(type, "colon", sep="_"))
 }
  
 get_tissue_name <- function(file, dataset, annotation) {
