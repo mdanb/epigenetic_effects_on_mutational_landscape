d8667a81820d1d94a4fcb0c1836a7d1a100cb4c4
bgiotti <brunogiotti@gmail.com>
2023-12-26 22:51:42 +0000

    f

--- analysis/ML/plot_top_features.R
+++ analysis/ML/plot_top_features.R
@@ -340,7 +340,7 @@ construct_robustness_boxplots <- function(df, x, y, title, savepath, savefile,
     
     # df_filtered["wrapped_y"] = str_wrap(df_filtered$y_reordered, width=15)
     p <- ggplot(df_filtered) +
-            geom_boxplot(aes(x = !!sym(x), y_reordered, fill=color), lwd = 1, 
+            geom_boxplot(aes(x = !!sym(x), y_reordered, fill=color), lwd = 0.3, 
                              outlier.shape = outlier_shape, outlier.size=2) +
             geom_text(aes(x = x_position + xlim_upper / 7,
                           y = y_reordered),
@@ -359,7 +359,7 @@ construct_robustness_boxplots <- function(df, x, y, title, savepath, savefile,
                 axis.text.x = element_text(size = 25),
                 axis.title.x=element_blank(),
                 axis.title.y=element_blank(),
-                axis.ticks = element_line(size=0.5),
+                axis.ticks = element_line(size=2),
                 axis.line = element_line(linewidth=0.5)
               ) +
             xlim(xlim_lower - xlim_lower / 10, xlim_upper + 2 * xlim_upper / 10)
