82bc8321a92b497ee6250301486b839a69b3c853
Brawni <brunogiotti@gmail.com>
2024-07-17 20:17:49 +0000

    add cd

--- paper/fig3.sh
+++ paper/fig3.sh
@@ -15,13 +15,18 @@
 # See Jupyter notebook analysis/ML/paper_umaps.ipynb 
 
 ### Fig 3D ###
-#Rscript ../analysis/ArchR_analysis/reannotate_datasets.R --cores=8 --dataset=Shendure --metadata_for_celltype_fn=GSE149683_File_S2.Metadata_of_high_quality_cells.txt --sep_for_metadata=\t --cell_type_col_in_metadata=cell_type --tissue=all --nfrags_filter=1 --tss_filter=0 --min_cells_per_cell_type=1 --filter_per_cell_type --plot_custom_column  --color_embedding_by=cell_type --get_metacells
+# cd ../analysis/ArchR_analysis
+#Rscript reannotate_datasets.R --cores=8 --dataset=Shendure --metadata_for_celltype_fn=GSE149683_File_S2.Metadata_of_high_quality_cells.txt --sep_for_metadata=\t --cell_type_col_in_metadata=cell_type --tissue=all --nfrags_filter=1 --tss_filter=0 --min_cells_per_cell_type=1 --filter_per_cell_type --plot_custom_column  --color_embedding_by=cell_type --get_metacells
+# cd ../../paper
 
-#Rscript ../data/scripts/create_count_overlaps.R --dataset=Shendure --cores=8 --annotation=default_annotation --overlaps_per_cell
+# cd ../data/scripts
+# Rscript create_count_overlaps.R --dataset=Shendure --cores=8 --annotation=default_annotation --overlaps_per_cell
+# cd ../../paper
 
-#Rscript ../analysis/correlation_analysis_clean.R --fig3_adeno
-#Rscript ../analysis/correlation_analysis_clean.R --fig3_neuro
-
+# cd ../analysis
+#Rscript correlation_analysis_clean.R --fig3_adeno
+#Rscript correlation_analysis_clean.R --fig3_neuro
+# cd ../paper
 
 ### Fig 3E ###
 #python3 ../analysis/ML/prep_ML_model_scripts.py --add_perf_to_file --add_p_to_file --cancer_types Panc-AdenoCA --datasets Bingren Shendure --scATAC_cell_number_filter=100 --annotation_dir=finalized_annotation --seed_interval=1-10 --fold_for_test_set_range=1-10 --seed_interval_step=5 --n_optuna_trials_prebackward_selection=50 --n_optuna_trials_backward_selection=50 --cores=8 --feature_importance_method=permutation_importance --test_set_perf_num_features all --top_features_to_plot_feat_imp 5 --tissues_to_consider "pancreas" "stomach" --woo_pcawg --submit_jobs
