a1899259555744f58b93b31897fdebaec5fb890f
leefall <leefall2@snu.ac.kr>
2025-08-11 14:29:15 -0400

    Update fig2.sh

--- paper/fig2.sh
+++ paper/fig2.sh
@@ -31,9 +31,9 @@
 #cd ../paper
 
 ### Fig 2d ###
-#python3 ../analysis/ML/prep_ML_model_scripts.py --cancer_types Lymph-CLL --datasets Greenleaf_pbmc_bm --scATAC_cell_number_filter=100 --annotation_dir=new_intermediate_blood_bm_annotation --seed_interval=1-10 --fold_for_test_set_range=1-10 --seed_interval_step=5 --n_optuna_trials_prebackward_selection=50 --n_optuna_trials_backward_selection=50 --cores=8 --feature_importance_method=permutation_importance --test_set_perf_num_features all --top_features_to_plot_feat_imp 5 --tissues_to_consider all --woo_pcawg --submit_jobs --add_p_to_file --add_perf_to_file
+#python3 ../analysis/ML/prep_ML_model_scripts.py --cancer_types Lymph-CLL --datasets Greenleaf_bm --scATAC_cell_number_filter=100 --annotation_dir=new_intermediate_blood_bm_annotation --seed_interval=1-10 --fold_for_test_set_range=1-10 --seed_interval_step=5 --n_optuna_trials_prebackward_selection=50 --n_optuna_trials_backward_selection=50 --cores=8 --feature_importance_method=permutation_importance --test_set_perf_num_features all --top_features_to_plot_feat_imp 5 --tissues_to_consider all --woo_pcawg --submit_jobs --add_p_to_file --add_perf_to_file
 # run robustness plotting as explained in README
 
-#python3 ../analysis/ML/prep_ML_model_scripts.py --cancer_types Myeloid-AML --datasets Greenleaf_pbmc_bm --scATAC_cell_number_filter=100 --annotation_dir=new_intermediate_blood_bm_annotation --seed_interval=1-10 --fold_for_test_set_range=1-10 --seed_interval_step=5 --n_optuna_trials_prebackward_selection=50 --n_optuna_trials_backward_selection=50 --cores=8 --feature_importance_method=permutation_importance --test_set_perf_num_features all --top_features_to_plot_feat_imp 5 --tissues_to_consider all --woo_pcawg --submit_jobs --add_p_to_file --add_perf_to_file
+#python3 ../analysis/ML/prep_ML_model_scripts.py --cancer_types Myeloid-AML --datasets Greenleaf_bm --scATAC_cell_number_filter=100 --annotation_dir=new_intermediate_blood_bm_annotation --seed_interval=1-10 --fold_for_test_set_range=1-10 --seed_interval_step=5 --n_optuna_trials_prebackward_selection=50 --n_optuna_trials_backward_selection=50 --cores=8 --feature_importance_method=permutation_importance --test_set_perf_num_features all --top_features_to_plot_feat_imp 5 --tissues_to_consider all --woo_pcawg --submit_jobs --add_p_to_file --add_perf_to_file
 # run robustness plotting as explained in README
 
